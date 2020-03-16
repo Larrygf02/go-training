e35223be56831bff12eeb1371183ad2045494586
larrygf02 <raulgf_02@hotmail.com>
2020-03-15 21:27:01 -0500

    Function recievers

--- functions/function.go
+++ functions/function.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Println(name)
 	age := getAges(33)
 	fmt.Println(age)
+	items(1,2,3,4)
 }
 
 // parameters
@@ -23,4 +24,13 @@ func getName(name string) string{
 
 func getAges(age int) int {
 	return age;
+}
+
+func items(x ...int) {
+	fmt.Println(x)
+	sum := 0
+	for _, value := range x {
+		sum += value
+	}
+	fmt.Println("Sum is, ", sum)
 }
