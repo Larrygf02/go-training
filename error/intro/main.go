c5ba630bf300235315812dcb064172e5abfd6fbe
larrygf02 <raulgf_02@hotmail.com>
2020-03-28 10:16:53 -0500

    Go os file err

--- error/intro/main.go
+++ error/intro/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
 
 func main() {
+	f, err := os.Create("names.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	r := strings.NewReader("Wassup")
+	io.Copy(f, r)
+}
+
+func gopanic() {
 	var answer int
 	fmt.Print("Name: ")
 	_, err := fmt.Scan(&answer)
