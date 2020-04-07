56ea382e754675352483c301c861d8d577f3e24e
larrygf02 <raulgf_02@hotmail.com>
2020-04-06 20:40:13 -0500

    Wait group in gorutine

--- gorutine/main.go
+++ gorutine/main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
+
+var wg sync.WaitGroup
 
 func compute(value int) {
 	for i := 0; i < value; i++ {
 		time.Sleep(time.Second)
 		fmt.Println(i)
 	}
+	wg.Done()
 }
 
 func main() {
 	fmt.Println("Go rutine")
 	// estas dos funciones se ejecutan en paralelo
+	wg.Add(1)
+	wg.Add(1)
 	go compute(5)
 	go compute(5)
-	fmt.Scanln()
+	wg.Wait()
 }
