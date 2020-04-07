f434e2c446f25bbd65e5529fdbacb3e540714f6f
larrygf02 <raulgf_02@hotmail.com>
2020-04-06 23:03:52 -0500

    Add client in jwt golang

---
+++ jwt/client/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+var mySignin = os.Getenv("MY_JWT_TOKEN")
+var mySigningKey = []byte(mySignin)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	validToken, err := GenerateJWT()
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+	fmt.Fprintf(w, validToken)
+}
+
+func GenerateJWT() (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["authorized"] = true
+	claims["user"] = "Raul"
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+	tokenString, err := token.SignedString(mySigningKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":9001", nil))
+}
+
+func main() {
+	fmt.Println("Simple API JWT")
+	/* tokenString, err := GenerateJWT()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	fmt.Println(tokenString) */
+	handleRequests()
+}
