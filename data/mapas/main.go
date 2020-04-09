558f2247dd425d4918d2bfec81eba61044d37502
larrygf02 <raulgf_02@hotmail.com>
2020-04-08 23:40:46 -0500

    Maps delete keys in golang

--- data/mapas/main.go
+++ data/mapas/main.go
@@ -7,4 +7,12 @@ func main() {
 	m["k1"] = 4
 	m["k2"] = 5
 	fmt.Println("map", m)
+	v1 := m["k1"]
+	fmt.Println("item", v1)
+	fmt.Println("longitud", len(m))
+	delete(m, "k2")
+	fmt.Println("map", m)
+	m["k1"] = 6
+	fmt.Println("map", m)
+	fmt.Println("item", v1)
 }
