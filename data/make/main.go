e3b56a2f3d93cce9b2eec7becf62b8638fe238e3
larrygf02 <raulgf_02@hotmail.com>
2020-04-08 23:28:45 -0500

    Copy slice golang

--- data/make/main.go
+++ data/make/main.go
@@ -8,5 +8,11 @@ func main() {
 	name[0] = "Grace"
 	name[1] = "Raul"
 	name[2] = "Maria"
+	name = append(name, "Yeli", "Ernesto")
 	fmt.Println("names", name)
+	fmt.Println("len", len(name))
+	// copia name
+	other_name := make([]string, len(name))
+	copy(other_name, name)
+	fmt.Println("Copia del arreglo name", other_name)
 }
