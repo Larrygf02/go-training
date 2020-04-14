ace146a2ec55ce6faab5b40fc41631be292067c5
larrygf02 <raulgf_02@hotmail.com>
2020-04-14 00:22:13 -0500

    Apply singlenton

--- patterns/singlenton/singlenton.go
+++ patterns/singlenton/singlenton.go
@@ -1,31 +1,45 @@
 package singlenton
 
+import "sync"
+
 var p *person
+var once sync.Once
 
 func GetInstance() *person {
-	if p == nil {
+	// se ejecuta una sola vez "Singlenton"
+	once.Do(func() {
 		p = &person{}
-	}
+	})
 	return p
 }
 
 type person struct {
 	name string
 	age  int
+	mux  sync.Mutex
 }
 
 func (p *person) SetName(name string) {
+	p.mux.Lock()
 	p.name = name
+	p.mux.Unlock()
 }
 
 func (p *person) GetName() string {
+	p.mux.Lock()
+	defer p.mux.Unlock()
 	return p.name
 }
 
 func (p *person) IncrementAge() {
+	p.mux.Lock()
 	p.age++
+	p.mux.Unlock()
 }
 
 func (p *person) GetAge() int {
+	p.mux.Lock()
+	defer p.mux.Unlock()
 	return p.age
+
 }
