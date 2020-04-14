a4d7d76e2b16d75654a8b03bc307e5075bdc1f6b
larrygf02 <raulgf_02@hotmail.com>
2020-04-14 00:12:07 -0500

    Operations concurrents in golang

--- patterns/patterns.go
+++ patterns/patterns.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/larrygf02/patterns/client_one"
 	"github.com/larrygf02/patterns/client_two"
@@ -9,10 +10,20 @@ import (
 )
 
 func main() {
+	wg := sync.WaitGroup{}
+	wg.Add(200)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			client_one.IncrementAge()
+		}()
+		go func() {
+			defer wg.Done()
+			client_two.IncrementAge()
+		}()
+	}
 	fmt.Println("Hello world")
-	client_one.IncrementAge()
-	client_two.IncrementAge()
-
+	wg.Wait()
 	p := singlenton.GetInstance()
 	age := p.GetAge()
 	fmt.Println(age)
