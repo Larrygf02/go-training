95da119c15c93f9282567bb374da9ea360dfc148
larrygf02 <raulgf_02@hotmail.com>
2020-04-14 21:12:10 -0500

    Practicando mas canales

--- channels/exercise/exercise1.go
+++ channels/exercise/exercise1.go
@@ -7,4 +7,14 @@ func main() {
 	go func() { messages <- "ping" }()
 	msg := <-messages
 	fmt.Println(msg)
+
+	text := make(chan string)
+	go func() { text <- "hello text" }()
+	mtext := <-text
+	fmt.Println(mtext)
+
+	numbers := make(chan int)
+	go func() { numbers <- 3 }()
+	num := <-numbers
+	fmt.Println(num)
 }
