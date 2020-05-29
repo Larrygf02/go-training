fe7b034dda76dd850ab2aec26c6f4d5a38361433
larrygf02 <raulgf_02@hotmail.com>
2020-05-28 22:06:28 -0500

    Go docker

---
+++ go-docker/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world golang other")
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", helloWorld).Methods("GET")
+	myRouter.Use(mux.CORSMethodMiddleware(myRouter))
+	log.Fatal(http.ListenAndServe(":8081", myRouter))
+}
+func main() {
+	fmt.Println("Multi Stage Dockerfile Tutorial")
+	handleRequests()
+}
